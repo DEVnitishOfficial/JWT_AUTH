84ad0b33d680d76b01cf3d397ed5c0f26b1644af
DEVnitishOfficial <nitish.naroun123@gmail.com>
2023-09-07 21:12:32 +0530

    cors added

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const app = express();
 const authRouter = require('./router/authRouter')
 const dbconnect = require('./config/dbConfig')
 const cookieParser = require('cookie-parser');
+const cors = require ('cors');
 dbconnect();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin : [process.env.CLIENT_URL],
+    credentials:true
+}))
 
 app.use('/api/auth',authRouter);
 app.use('/', (req,res) => {
